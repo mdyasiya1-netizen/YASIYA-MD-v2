bf3227493d1f30770836cfd2b046c8c420aeee58
mdyasiya1-netizen <mdyasiya1@gmail.com>
2026-01-15 20:59:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?0Axq4J'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?6pOq4H'
 };
